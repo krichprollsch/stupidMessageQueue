3a054a23fe53546dc9bec862fb868dbbe6e51951
Pierre Tachoire <ptachoire@itnetwork.fr>
2012-11-05 15:39:30 +0100

    add lib exceptions

---
+++ src/stupidMessageQueue/Exception/ExceptionInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *  05/11/12 14:40
+ */
+
+namespace stupidMessageQueue\Exception;
+
+/**
+ * ExceptionInterface
+ *
+ * @author pierre
+ */
+interface ExceptionInterface
+{
+
+}
