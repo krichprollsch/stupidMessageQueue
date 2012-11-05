27931e78a4ab85a74f33a0d4eb2bf34f00f9e6d8
Pierre Tachoire <ptachoire@itnetwork.fr>
2012-11-05 15:41:07 +0100

    add lib exceptions

---
+++ src/stupidMessageQueue/Exception/UnexpectedValueException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *  05/11/12 14:41
+ */
+
+namespace stupidMessageQueue\Exception;
+
+/**
+ * InvalidArgumentException
+ *
+ * @author pierre
+ */
+class UnexpectedValueException extends \UnexpectedValueException implements ExceptionInterface
+{
+
+}
